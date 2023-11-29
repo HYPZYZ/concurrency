94f0814469b51b5fde2fbee4a89035b795c2cb20
Rik R <rik@remijn.org>
2023-11-29 23:06:41 +0100

    w1

---
+++ solutions/w1/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Counter counter = new Counter();
+        
+        counter.CountTo(10);
+        
+        int finalCount = counter.GetCount();
+        Console.WriteLine($"Final Count: {finalCount}");
+    }
+}
+
+public class Counter
+{
+    private int _count = 0;
+
+    public void CountTo(int target)
+    {
+        for (int i = 0; i < target; i++)
+        {
+            _count++;
+            Console.WriteLine(_count);
+        }
+    }
+
+    public int GetCount()
+    {
+        return _count;
+    }
+}
