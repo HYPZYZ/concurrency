64df0a740a219ae23e34f3e45f59754f24502916
Rik R <rik@remijn.org>
2023-11-29 23:23:37 +0100

    w2

---
+++ solutions/w2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+/*
+
++Which namespace is needed?
+System? Threading?
+
++Which class and method are used to run a process?
+Process, Process.Start()
+
++How can we modify properties of a process?
+with class ProcessStartInfo and pass to Process.Start(params)
+
++How can we get a list of currently running processes?
+Process.GetProcesses
+
+*/
